d27f73c86cee49fdd3a464f9ffe70ffc2ac428f3
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2023-05-29 23:24:17 -0300

    chore: Teste snapshot Input

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Input} from './';
+
+describe('<Input />', () => {
+  it('Should take Snapshot', () => {
+    const snapshot = renderer.create(<Input />).toJSON();
+    expect(snapshot).toMatchSnapshot();
+  });
+});
