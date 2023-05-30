d04eed10d2741912ae84fdb9b475470e4dca5984
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2023-05-29 23:16:12 -0300

    chore: Testes unitários

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Button} from './';
+import {Text, TouchableOpacity} from 'react-native';
+
+const mockFn = jest.fn();
+
+const props = {
+  text: 'Entrar',
+};
+
+const propsLoading = {
+  text: 'Entrar',
+  loading: true,
+};
+
+const propsNotPrimary = {
+  text: 'Entrar',
+  primary: false,
+  onPress: mockFn,
+};
+
+describe('<Button />', () => {
+  it('Should render corretly', () => {
+    const button = renderer.create(<Button {...props} />);
+    expect(button).toBeTruthy();
+  });
+
+  it('Should render corretly with loading', () => {
+    const button = renderer.create(<Button {...propsLoading} />);
+    expect(button).toBeTruthy();
+  });
+
+  it('Should render corretly with primary false', () => {
+    const button = renderer.create(<Button {...propsNotPrimary} />);
+    expect(button).toBeTruthy();
+  });
+
+  it('Should render corretly with text', () => {
+    const inst = renderer.create(<Button {...propsNotPrimary} />);
+    const text = inst.root.findByType(Text);
+    expect(text.props.children).toBe('Entrar');
+  });
+
+  it('Should render corretly with onPress()', () => {
+    const inst = renderer.create(<Button {...propsNotPrimary} />);
+    const button = inst.root.findByType(TouchableOpacity);
+    button.props.onPress();
+    expect(button.props.onPress).toBeCalledTimes(1);
+    expect(button).toBeTruthy();
+  });
+
+  it('Should take Snapshot', () => {
+    const snapshot = renderer.create(<Button {...props} />).toJSON();
+    expect(snapshot).toMatchSnapshot();
+  });
+});
---
+++ src/components/Divider/Divider.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Divider} from './';
+
+const props = {
+  size: 16,
+};
+
+const propsHorizontal = {
+  size: 16,
+  horizontal: true,
+};
+
+describe('<Divider />', () => {
+  it('Should render corretly', () => {
+    const divider = renderer.create(<Divider {...props} />);
+    expect(divider).toBeTruthy();
+  });
+
+  it('Should render corretly with horizontal', () => {
+    const divider = renderer.create(<Divider {...propsHorizontal} />);
+    expect(divider).toBeTruthy();
+  });
+
+  it('Should render corretly with size', () => {
+    const inst = renderer.create(<Divider {...propsHorizontal} />);
+    const size = inst.root.props.size;
+    expect(size).toBe(16);
+  });
+
+  it('Should take Snapshot', () => {
+    const snapshot = renderer.create(<Divider {...props} />).toJSON();
+    expect(snapshot).toMatchSnapshot();
+  });
+});
