dda4c9500cfbb7c56b74c2535b56856057cd7f46
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2023-05-30 13:35:38 -0300

    chore: Adicionando SplashScreen

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
 
 // Routes
 import {Routes} from '@routes';
@@ -12,6 +13,10 @@ import {store} from '@store';
 import {$COLORS} from './utils/colors';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <StatusBar
